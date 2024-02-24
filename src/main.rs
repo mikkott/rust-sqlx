993401694d8de25a6c653c4aa4d72aeba54585e9
mikkott <mikko@email>
2024-02-24 17:21:31 +0200

    add delete fn

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,27 @@ pub mod models;
 async fn main() -> anyhow::Result<()> {
     let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
 
-    let foo = add_user(&pool, User::new("foo".to_string(), "foo@bar".to_string())).await?;
+    let foo = add_user(&pool, &User::new("foo".to_string(), "foo@bar".to_string())).await?;
     println!("Added user {}", foo);
 
     let bar = list_users::<User>(&pool).await?;
 
+    for e in &bar {
+        println!("Deleting: {:?}", e);
+        delete_user(&pool, e).await?;
+    }
+
+    let bar = list_users::<User>(&pool).await?;
+
     for e in bar {
+        println!("Users:");
         println!("{:?}", e);
     }
 
     Ok(())
 }
 
-async fn add_user(pool: &SqlitePool, user: User) -> anyhow::Result<i64> {
+async fn add_user(pool: &SqlitePool, user: &User) -> anyhow::Result<i64> {
     let mut conn = pool.acquire().await?;
     let id = sqlx::query!(
         r#"
@@ -42,4 +50,14 @@ async fn list_users<T>(pool: &SqlitePool) -> anyhow::Result<Vec<User>> {
         .await?;
 
     Ok(users)
+}
+
+async fn delete_user(pool: &SqlitePool, user: &User) -> anyhow::Result<u64> {
+    let mut conn = pool.acquire().await?;
+    let id = sqlx::query!(r#"DELETE FROM users WHERE id = ?1"#, user.id)
+        .execute(&mut *conn)
+        .await?
+        .rows_affected();
+
+    Ok(id)
 }

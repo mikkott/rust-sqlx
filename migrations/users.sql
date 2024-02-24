957a5a74c0bf04f78e35d907731f1c1cb9cbbf26
mikkott <mikko@email>
2024-02-24 15:23:18 +0200

    add sqlite

---
+++ migrations/users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY NOT NULL,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    enabled BOOLEAN NOT NULL DEFAULT 0
+);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::sqlite::SqlitePool;
+use std::env;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+
+    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
+
+    Ok(())
 }
+
